d6ab640ba806a07feb2c81635312e7fbbb004100
Ayush Gupta <127961367+AyusGup@users.noreply.github.com>
2024-04-12 20:57:57 +0530

    Update Dashboard.js

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -5,21 +5,7 @@ const Dashboard = (props) => {
   const [id, setId] = useState("");
   const location = useLocation();
 
-  useEffect(() => {
-    const searchParams = new URLSearchParams(location.search);
-
-    const accessToken = searchParams.get("access_token");
-    const expiresIn = searchParams.get("expires_in");
-    const username = searchParams.get("username");
-    if (accessToken && expiresIn && username) {
-      window.location.href = `http://localhost:3000?access_token=${accessToken}&expires_in=${expiresIn}&username=${username}`;
-    }
-  }, [location]);
-
-  const hiveSigner = (e) => {
-    e.preventDefault();
-    window.location.href = `https://hivesigner.com/oauth2/authorize?client_id=${id}&redirect_uri=http%3A%2F%2Flocalhost%3A3000&scope=vote,comment,custom_json`;
-  };
+ 
   const LoginBoxElement = document.getElementById("login-Box");
   if (LoginBoxElement)
     LoginBoxElement.style.display = props.visibity === true ? "block" : "none";
@@ -33,7 +19,7 @@ const Dashboard = (props) => {
         id="login-Box"
       >
         <div className="w-full h-full flex items-center justify-center">
-          <form className="flex flex-col space-y-4" onSubmit={hiveSigner}>
+          <form className="flex flex-col space-y-4">
             <div className="flex">
               <label htmlFor="id" className="text-3xl">
                 @
